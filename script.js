403b133dd5756e362763a491aa5a95e8938336e6
Caroline <wanguic49@gmail.com>
2024-05-13 14:31:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+function onSubmit(event){
+    event.preventDefault();
+
+    let formData = new FormData(document.getElementById('taskForm'));
+    let taskDiv = document.createElement('div');
+
+    for(let [name,value] of formData.entries()){
+        let p = document.createElement('p');
+        p.textContent = name + ': ' + value;
+        taskDiv.appendChild(p);
+    }
+
+ 
+    let tasksDiv = document.getElementById('tasks');
+    tasksDiv.appendChild(taskDiv);
+
+    document.getElementById('task-form').style.display = 'none';
+    tasksDiv.style.display = '';
+
+}
+
+document.getElementById('taskForm').addEventListener('submit',onSubmit);
+
+function newTask(){
+    document.getElementById('task-form').style.display = '';
+    document.getElementById('tasks').style.display = 'none';
+}
